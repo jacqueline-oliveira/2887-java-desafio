63d840dfb55e8253a5d87c273615a22631107a69
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-03-15 16:27:00 -0300

    video 5.3

---
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -0,0 +1,33 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setApresentador("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+    }
+}
